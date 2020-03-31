209c6bf2dfebb86b1b86122d5a99378a78f711ed
Alex Costigan <alexcostigan@icloud.com>
2020-03-31 17:46:22 +0100

    Updates

---
+++ precache-manifest.e3407e58556dce656e8fa0e2eb8ef556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0cc0767a324894ae1653f254e9eded",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "4564aaa765bd9ab9f639",
+    "url": "/todolist-react/static/css/main.761edb03.chunk.css"
+  },
+  {
+    "revision": "a1cef20e4212fffcaf7c",
+    "url": "/todolist-react/static/js/2.05d7d09e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.05d7d09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4564aaa765bd9ab9f639",
+    "url": "/todolist-react/static/js/main.8d350497.chunk.js"
+  },
+  {
+    "revision": "470470180985e24745a2",
+    "url": "/todolist-react/static/js/runtime-main.1b8b8966.js"
+  }
+]);
